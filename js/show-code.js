a79c57a72c5a8ce9f4347503094cb2446e0ee579
Sunny Golovine <sunny@golovinemail.org>
2016-09-28 20:21:34 -0400

    git commit -m

---
+++ js/show-code.js
@@ -0,0 +1,20 @@
+function show_go_get() {
+    document.getElementById('code-area').innerHTML = "<code>go get https://github.com/Go4Algorithms/golang</code>";
+}
+function show_go_git() {
+    document.getElementById('code-area').innerHTML = "<code>git clone https://github.com/Go4Algorithms/golang</code>";
+}
+function show_py_git() {
+    document.getElementById('code-area').innerHTML = "<code>git clone https://github.com/Go4Algorithms/python</code>";
+}
+function show_download() {
+  document.getElementById('code-area').innerHTML =
+
+ '<a href="https://github.com/Go4Algorithms/golang/archive/master.zip" class="btn btn-info" role="button">Link Button</a>' +
+ '&nbsp;&nbsp;&nbsp;' +
+ '<a href="https://github.com/Go4Algorithms/python/archive/master.zip" class="btn btn-info" role="button">Link Button</a>'
+
+}
+function reset() {
+  document.getElementById('code-area').innerHTML = ""
+}
